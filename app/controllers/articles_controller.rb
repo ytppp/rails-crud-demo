dd6f6ecd904b8b10cd8d6bcfcecb2a7783e0b587
ytppp <651003708@qq.com>
2023-07-18 14:33:46 +0800

    feat: update operation of  article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,27 @@
 class ArticlesController < ApplicationController
+  
+  # index，show，new，edit，create，update and destroy
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(articles_params)
+
+    @article.save
+    redirect_to @article
   end
+
+  private
+    def articles_params
+      params.require(:article).permit(:title, :text)
+    end
 end
