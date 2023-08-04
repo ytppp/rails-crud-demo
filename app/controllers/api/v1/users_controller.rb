70bb705d3d0bba2800c0ae58bdd6f29ba4a5233c
ytppp <651003708@qq.com>
2023-08-04 17:48:28 +0800

    feat: add users_controller show action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,6 +7,10 @@ class Api::V1::UsersController < ApplicationController
     render json: {error_code:0, data:@users, message:'ok'}, status: 200
   end
 
+  def show
+    render json: {error_code:0, data: @user, message: 'ok'}, status: 200
+  end
+
   private
     def _to_i(param, default_no = 1)
       param && param&.to_i > 0 ? param&.to_i : default_no.to_i
@@ -19,5 +23,10 @@ class Api::V1::UsersController < ApplicationController
     def set_page
       @page = _to_i(params[:page], 1)
       @page = set_per_page * (@page - 1)
+    end
+
+    def set_user
+      @user = User.find_by_id params[:id].to_i
+      @user = @user || {}
     end
 end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,8 +1,19 @@
 require "test_helper"
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:one)
+  end
   test "index_success: should show users" do
     get api_v1_users_path, as: :json
     assert_response 200
+  end
+  test "show_success: should show user" do
+    get api_v1_user_path(@user), as: :json
+    json_response = JSON.parse(self.response.body)
+    # 验证状态码
+    assert_response 200
+    # 验证返回数据
+    assert_equal @user.email, json_response['data']['email']
   end
 end
