f087c4889c417265273f1d307faee5e2915255dd
ytppp <651003708@qq.com>
2023-08-11 15:30:32 +0800

    feat: update email validates rule

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true, format: { with: /\w+@\w+\.{1}[a-zA-Z]{2,}/ }
+  validates :email, presence: true, uniqueness: true, format: { with: /[a-zA-Z0-9_-]+@[a-zA-Z0-9_-]+(.[a-zA-Z0-9_-]+)+/ } // /\w+@\w+\.{1}[a-zA-Z]{2,}/
   validates :password_digest, presence: true
   validates :role, inclusion: { in: [0, 1, 2], message: "role can be only in [0 1 2]" }
   
