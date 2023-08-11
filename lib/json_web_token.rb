c1b2ec764a9beb92bdc369c385e9d9b37c1f7eed
ytppp <651003708@qq.com>
2023-08-11 15:25:10 +0800

    feat: update 'form_now' to 'from_now'

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,7 +1,7 @@
 class JsonWebToken
   SECRET_KEY = Rails.application.credentials.secret_key_base.to_s
 
-  def self.encode(payload, exp = 24.hours.form_now)
+  def self.encode(payload, exp = 24.hours.from_now)
     payload[:exp] = exp.to_i
     JWT.encode(payload, SECRET_KEY)
   end
