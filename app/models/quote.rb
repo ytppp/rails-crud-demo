e4dee7d0394b32c408e0c7b4937a8a3be267b578
ytppp <651003708@qq.com>
2023-09-04 11:10:10 +0800

    feat: add turbo stream

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -2,4 +2,11 @@ class Quote < ApplicationRecord
   validates :name, presence: true
 
   scope :ordered, -> { order(id: :desc) }
+
+  # after_create_commit -> { broadcast_prepend_later_to "quotes" }
+  # after_update_commit -> { broadcast_replace_later_to "quotes" }
+  # after_destroy_commit -> { broadcast_remove_to "quotes" }
+  # 上面等价于下面
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
 end
+Quote.first.update!(name: "Update from console")
